cd1f444363596ea97ff993fd39dc80d5737d8152
Robert Ku <robertku79@gmail.com>
2020-01-04 18:19:59 -0500

    Add auth

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+const passport = require('passport');
+
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 const express = require("express"); //create server
 const app = express();
+
+const users = require("./routes/api/users"); // import routes
+const tweets = require("./routes/api/tweets");
 
 const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    
-app.get("/", (req, res) => res.send("Hello World")); //create route
+
+// app.get("/", (req, res) => res.send("Hello World")); //create route
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 const port = process.env.PORT || 5000; //choose port
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users); //use routes
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false })); //middleware
+app.use(bodyParser.json());
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A Twitter Clone</h1>
+                <footer>
+                    2020
+        </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
