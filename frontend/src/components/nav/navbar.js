ce861d13471f754d682a42cf72913eadba518e3a
Robert Ku <robertku79@gmail.com>
2020-01-06 15:30:41 -0500

    Fix login

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -39,7 +39,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <h1>Chirper</h1>
+                <h1>Cheeper</h1>
                 {this.getLinks()}
             </div>
         );
